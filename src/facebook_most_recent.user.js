35583fab41dd328d680640e4e8411b40e1d4cd21
Y-CChen <250f42mt@duck.com>
2024-02-02 14:47:17 +0800

    Update src/facebook_most_recent.user.js

--- src/facebook_most_recent.user.js
+++ src/facebook_most_recent.user.js
@@ -3,31 +3,39 @@
 // @namespace   https://github.com/Y-CChen/userscripts
 // @downloadURL https://raw.githubusercontent.com/Y-CChen/userscripts/main/src/facebook_most_recent.user.js
 // @updateURL   https://raw.githubusercontent.com/Y-CChen/userscripts/main/src/facebook_most_recent.user.js
-// @version     0.1
+// @version     0.20240202.0
 // @description Facebook automatically change News Feed to Most Recent
 // @author      CYCheng
 // @match       https://www.facebook.com/*
 // @run-at      document-start
 // ==/UserScript==
 
+// cSpell:ignore history.onpushstate
+
 (function (document) {
   'use strict';
 
-  const logEnabled = false;
+  const options = {
+    logEnabled: false,
+  };
+
+  const utils = {
+    log: options.logEnabled ? console.log : () => {},
+  };
+
   const chr = 'sk=h_chr';
-  const log = logEnabled ? console.log : () => {};
   const gotoChr = () => {
     if (document.location.pathname !== '/') {
-      log(document.location.pathname);
+      utils.log(document.location.pathname);
       return;
     }
     if (!document.location.href.includes(chr)) {
-      log('goto chr');
-      document.location.replace('https://www.facebook.com/?' + chr);
+      utils.log('goto chr');
+      document.location.replace(`https://www.facebook.com/?${chr}`);
     }
   };
 
-  log('start');
+  utils.log('start');
   const pushState = history.pushState;
   history.pushState = function (state) {
     if (typeof history.onpushstate === 'function') {
